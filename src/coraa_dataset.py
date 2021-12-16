ca62b2b01e88207f861a0efa5d62bce6e8dfe030
Gabriel Ziegler <gabrielziegler3@gmail.com>
2021-12-16 12:12:00 -0300

    Add resampler and return wave signal
    Signed-off-by: Gabriel Ziegler <gabrielziegler3@gmail.com>

--- src/coraa_dataset.py
+++ src/coraa_dataset.py
@@ -1,7 +1,7 @@
 import re
 import pathlib
 import torch
-import os
+import torchaudio
 
 from pathlib import Path
 from torch.utils.data import Dataset
@@ -9,24 +9,38 @@ from torch.utils.data import Dataset
 
 class CORAADataset(Dataset):
 
-    def __init__(self, data_dir: str):
+    def __init__(self, data_dir: str, model_sr: int):
         self.audio_files = self._get_audio_files(data_dir)
         self.labels = self._get_labels()
+        self.model_sr = model_sr
 
     def __len__(self):
         return len(self.labels)
 
     def __getitem__(self, idx: int) -> tuple:
         audio_sample = self.audio_files[idx]
+        audio_signal = self._get_audio_signal(audio_sample)
+
         label_sample = self.labels[idx]
-        return audio_sample, label_sample
+
+        return audio_signal, label_sample
 
     def _get_audio_files(self, data_dir: str) -> list:
         p = Path(data_dir).glob('**/*')
-        audio_files = [x.name for x in p if x.is_file()]
+        audio_files = [str(x.resolve()) for x in p if x.is_file()]
         audio_files = sorted(audio_files)
-        print(audio_files)
+
         return audio_files
+
+    def _get_audio_signal(self, filename):
+        signal, sr = torchaudio.load(filename)
+
+        if sr != self.model_sr:
+            resampler = torchaudio.transforms.Resample(sr, self.model_sr)
+            signal = resampler(signal)
+
+        return signal
+
 
     def _get_labels(self) -> list:
         def extract_label_from_filename(filename):
@@ -34,11 +48,10 @@ class CORAADataset(Dataset):
             return label
 
         labels = [extract_label_from_filename(filename) for filename in self.audio_files]
-        print(labels)
         return labels
 
 
 if __name__ == "__main__":
     DATA_DIR = "../data/train/"
-    dataset = CORAADataset(DATA_DIR)
+    dataset = CORAADataset(DATA_DIR, model_sr=16000)
 
